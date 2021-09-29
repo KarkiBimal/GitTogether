3563d8aa7c969f532edf2ffa9a765f4ed8a73760
Bimal <karkibimal0@gmail.com>
2021-09-29 00:37:44 -0500

    All Changes

--- app/src/main/java/com/example/gittogether/LoginActivity.java
+++ app/src/main/java/com/example/gittogether/LoginActivity.java
@@ -1,14 +1,142 @@
 package com.example.gittogether;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button button, signUp, logIn;
+    private TextView forgotPassword;
+    private FirebaseAuth auth;
+    private EditText inputEmail, inputPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        inputEmail = (EditText) findViewById(R.id.et_email);
+        inputPassword = (EditText) findViewById(R.id.et_password);
+
+        auth = FirebaseAuth.getInstance();
+
+
+
+        signUp = (Button) findViewById(R.id.bt_signup);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), SignUp.class));
+            }
+        }
+        );
+
+
+
+        logIn = (Button) findViewById(R.id.bt_login);
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = inputEmail.getText().toString().trim();
+                final String password = inputPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                // If sign in fails, display a message to the user. If sign in succeeds
+                                // the auth state listener will be notified and logic to handle the
+                                // signed in user can be handled in the listener.
+                                if (task.isSuccessful()) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(LoginActivity.this, "Logged in Sucessfully", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                                    } else {
+                                        Toast.makeText(LoginActivity.this, "Failed to Login" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+
+
+                            }
+
+                        });
+            }
+        });
+
+
+
+
+        forgotPassword = (TextView) findViewById(R.id.forgot_password);
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText resetMail = new EditText(view.getContext());
+                AlertDialog.Builder passwordResetDialogue = new AlertDialog.Builder(view.getContext());
+                passwordResetDialogue.setTitle("Reset Password?");
+                passwordResetDialogue.setMessage("Enter Your Email To Receive Reset Link.");
+                passwordResetDialogue.setView(resetMail);
+
+                passwordResetDialogue.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String mail= resetMail.getText().toString();
+                    auth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(LoginActivity.this, "Rest Link Sent To Your Email", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(LoginActivity.this,"Error! Reset link is not sent."+e.getMessage(),Toast.LENGTH_SHORT);
+                        }
+                    });
+                    }
+                });
+                passwordResetDialogue.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                passwordResetDialogue.create().show();
+            }
+        });
+
+
+
+
     }
 }
+
--- app/src/main/java/com/example/gittogether/SignUp.java
+++ app/src/main/java/com/example/gittogether/SignUp.java
@@ -1,14 +1,132 @@
 package com.example.gittogether;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class SignUp extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.regex.Pattern;
+
+public class SignUp extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText editTextFirstName,  editTextLastName,  editTextEmail,  editTextPassword, editTextRepeatPassword;
+    private Button registerUser;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        mAuth = FirebaseAuth.getInstance();
+
+        registerUser=(Button)findViewById(R.id.bt_register);
+        registerUser.setOnClickListener(this);
+
+        editTextEmail=(EditText) findViewById(R.id.et_email);
+        editTextEmail.setOnClickListener(this);
+
+        editTextFirstName=(EditText) findViewById(R.id.et_first_name);
+        editTextFirstName.setOnClickListener(this);
+
+        editTextLastName=(EditText) findViewById(R.id.et_last_name);
+        editTextLastName.setOnClickListener(this);
+
+        editTextPassword=(EditText) findViewById(R.id.et_password);
+        editTextPassword.setOnClickListener(this);
+
+        editTextRepeatPassword=(EditText) findViewById(R.id.et_repeat_password);
+        editTextRepeatPassword.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.bt_register:
+                registerUser();
+                break;
+        }
+    }
+    private void registerUser(){
+
+        String email=editTextEmail.getText().toString().trim();
+        String firstName=editTextFirstName.getText().toString().trim();
+        String lastName=editTextLastName.getText().toString().trim();
+        String password=editTextPassword.getText().toString().trim();
+        String repeatPassword=editTextRepeatPassword.getText().toString().trim();
+
+        if(FirebaseAuth.getInstance().getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+        }
+
+        if(firstName.isEmpty()){
+            editTextFirstName.setError("You Idiot....");
+            editTextFirstName.requestFocus();
+            return;
+        }
+        if(lastName.isEmpty()){
+            editTextLastName.setError("You Idiot....");
+            editTextLastName.requestFocus();
+            return;
+        }
+        if(email.isEmpty()){
+            editTextEmail.setError("You Idiot....");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("You are Super Idiot....");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            editTextPassword.setError("You Idiot....");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if(repeatPassword.isEmpty()){
+            editTextRepeatPassword.setError("You Idiot....");
+            editTextRepeatPassword.requestFocus();
+            return;
+        }
+
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            User user=new User(firstName, lastName, email);
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(SignUp.this, "User Registered", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                                    }
+                                    else{
+                                        Toast.makeText(SignUp.this, "User Not Registered", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        }
+                    }
+                });
+
+
+    }
+
 }
