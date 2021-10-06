9c54ece2b49d81714f8b15311b732cf4547ce827
Bimal <karkibimal0@gmail.com>
2021-10-06 15:31:21 -0500

    Profile Page Changes

--- app/src/main/java/com/example/gittogether/MainActivity.java
+++ app/src/main/java/com/example/gittogether/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.gittogether;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView profileImage;
+    TextView changePic;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        profileImage= findViewById(R.id.profile_pic);
+        changePic=findViewById(R.id.change_pic);
+
+
+
+
+        changePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent openGalleryIntent= new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(openGalleryIntent, 1000);
+            }
+        });
     }
 }
