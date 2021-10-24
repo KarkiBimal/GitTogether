858cdec095c7c9d12500f93ec54fd07674d2e53d
Daniel <daniel.stephens1@yahoo.com>
2021-10-23 20:29:42 -0500

    Moved navigation drawer to separate class added a "Post" page temporarily

---
+++ app/src/main/java/com/example/gittogether/Navigation.java
@@ -0,0 +1,107 @@
+package com.example.gittogether;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Navigation extends AppCompatActivity {
+    //Initialize variable
+    DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+
+    public void ClickMenu(View view){
+        //Open drawer
+        openDrawer(drawerLayout);
+    }
+
+    public static void openDrawer(DrawerLayout drawerLayout) {
+        //Open drawer layout
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    public void ClickLogo(View view){
+        //Close drawer
+        closeDrawer(drawerLayout);
+    }
+
+    public static void closeDrawer(DrawerLayout drawerLayout) {
+        //Close drawer layout
+        //Check condition
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            //When drawer is open
+            //Close drawer
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    public void ClickHome(View view){
+        //Redirect activity to Home
+        redirectActivity(this, MainActivity.class);
+    }
+
+        public void ClickPost(View view){
+            //Redirect activity to post
+            redirectActivity(this, PostActivity.class);
+        }
+
+    public void ClickProfile(View view){
+        //Redirect activity to profile
+        redirectActivity(this, ProfilePage.class);
+    }
+
+    public void ClickLogout(View view){
+        //close app
+        logout(this);
+    }
+
+    public static void logout(Activity activity) {
+        //Initialize alert dialogue
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        //set title
+        builder.setTitle("Logout");
+        //set message
+        builder.setMessage("Are you sure you want to logout?");
+        //positive yes button
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //finish activity
+                activity.finishAffinity();
+                //exit app
+                System.exit(0);
+            }
+        });
+        //Negative no button
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Dismiss dialogue
+                dialog.dismiss();
+            }
+        });
+        //show dialogue
+        builder.show();
+    }
+
+    public static void redirectActivity(Activity activity, Class aClass) {
+        //Initialize intent
+        Intent intent = new Intent(activity, aClass);
+        //set flag
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //start activity
+        activity.startActivity(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //close drawer
+        closeDrawer(drawerLayout);
+    }
+}
