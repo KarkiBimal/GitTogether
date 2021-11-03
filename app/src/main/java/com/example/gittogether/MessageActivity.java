254ac844cb73fa9a2ba9144c534886a8688538f9
Razan-8 <64858643+Razan-8@users.noreply.github.com>
2021-11-03 12:20:31 -0500

    fixup! slight MessageActivity fixes

--- app/src/main/java/com/example/gittogether/MessageActivity.java
+++ app/src/main/java/com/example/gittogether/MessageActivity.java
@@ -37,101 +37,101 @@ import java.util.Map;
 
 public class MessageActivity extends AppCompatActivity {
 
-        LinearLayout layout;
-        ImageView sendButton;
-        EditText messageArea;
-        ScrollView scrollView;
-        DrawerLayout drawerLayout;
+    LinearLayout layout;
+    ImageView sendButton;
+    EditText messageArea;
+    ScrollView scrollView;
+    DrawerLayout drawerLayout;
 
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(activity_message);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_message);
 
-            layout = (LinearLayout)findViewById(R.id.layout1);
-            sendButton = (ImageView)findViewById(R.id.sendButton);
-            messageArea = (EditText)findViewById(R.id.messageArea);
-            scrollView = (ScrollView)findViewById(R.id.scrollView);
-            Firebase reference1;
-            Firebase reference2;
+        layout = (LinearLayout)findViewById(R.id.layout1);
+        sendButton = (ImageView)findViewById(R.id.sendButton);
+        messageArea = (EditText)findViewById(R.id.messageArea);
+        scrollView = (ScrollView)findViewById(R.id.scrollView);
+        Firebase reference1;
+        Firebase reference2;
 
-            Firebase.setAndroidContext(this);
-            reference1 = new Firebase("https://gittogether-13f65-default-rtdb.firebaseio.com/Message" + /*+*/  "_" /*+*/);
-            reference2 = new Firebase("https://gittogether-13f65-default-rtdb.firebaseio.com/Message" + /*+*/  "_" /*+*/);
+        Firebase.setAndroidContext(this);
+        reference1 = new Firebase("https://gittogether-13f65-default-rtdb.firebaseio.com/Message" + /*+*/  "_" /*+*/);
+        reference2 = new Firebase("https://gittogether-13f65-default-rtdb.firebaseio.com/Message" + /*+*/  "_" /*+*/);
 
 
-            sendButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    String messageText = messageArea.getText().toString();
-                    //TextView name = (TextView) findViewById(id.name);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String messageText = messageArea.getText().toString();
+                //TextView name = (TextView) findViewById(id.name);
 
-                    if(!messageText.equals("")){
-                        Map<String, String> map = new HashMap<String, String>();
-                        map.put("message", messageText);
-                        map.put("user", FirebaseDatabase.class.getName());
-                    }
-
+                if(!messageText.equals("")){
+                    Map<String, String> map = new HashMap<String, String>();
+                    map.put("message", messageText);
+                    map.put("user", FirebaseDatabase.class.getName());
                 }
 
-            });
-
-            reference1.addChildEventListener(new ChildEventListener() {
-                @Override
-                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                    Map map = dataSnapshot.getValue(Map.class);
-                    String message = map.get("message").toString();
-                    String userName = map.get("user").toString();
-
-                    if(userName.equals(User.class.getName())){
-                        addMessageBox("You:\n" + message, 1);
-                    }
-                    else{
-                        addMessageBox(/*User.class.chatWith + */ ":-\n" + message, 2);
-                    }
-                }
-
-                @Override
-                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-                }
-
-                @Override
-                public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-                }
-
-                @Override
-                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-                }
-
-                @Override
-                public void onCancelled(FirebaseError firebaseError) {
-
-                }
-            });
-        }
-
-        public void addMessageBox(String message, int type){
-            TextView textView = new TextView(MessageActivity.this);
-            textView.setText(message);
-            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams
-                    (ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-            lp.setMargins(0, 0, 0, 10);
-            textView.setLayoutParams(lp);
-
-            if(type == 1) {
-                textView.setBackgroundResource(R.drawable.rounded_corner1);
-            }
-            else{
-                textView.setBackgroundResource(R.drawable.rounded_corner2);
             }
 
-            layout.addView(textView);
-            scrollView.fullScroll(View.FOCUS_DOWN);
+        });
+
+        reference1.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Map map = dataSnapshot.getValue(Map.class);
+                String message = map.get("message").toString();
+                String userName = map.get("user").toString();
+
+                if(userName.equals(User.class.getName())){
+                    addMessageBox("You:\n" + message, 1);
+                }
+                else{
+                    addMessageBox(/*User.class.chatWith + */ ":-\n" + message, 2);
+                }
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+    }
+
+    public void addMessageBox(String message, int type){
+        TextView textView = new TextView(MessageActivity.this);
+        textView.setText(message);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams
+                (ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(0, 0, 0, 10);
+        textView.setLayoutParams(lp);
+
+        if(type == 1) {
+            textView.setBackgroundResource(R.drawable.rounded_corner1);
         }
+        else{
+            textView.setBackgroundResource(R.drawable.rounded_corner2);
+        }
+
+        layout.addView(textView);
+        scrollView.fullScroll(View.FOCUS_DOWN);
+    }
 
 
     /*
