14711b46cca06558758481e2c27fe06a96c3795d
Razan-8 <64858643+Razan-8@users.noreply.github.com>
2021-11-03 09:50:12 -0500

    Deleted previous and added new messages files.

--- app/src/main/java/com/example/gittogether/MessageActivity.java
+++ app/src/main/java/com/example/gittogether/MessageActivity.java
@@ -8,11 +8,17 @@ import androidx.core.widget.TextViewCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
+import com.firebase.client.Firebase;
 import com.firebase.ui.database.FirebaseListAdapter;
 import com.firebase.ui.database.FirebaseListOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -20,127 +26,112 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.FirebaseError;
+
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MessageActivity extends AppCompatActivity {
 
-    private FirebaseListAdapter<ChatMessage> adapter;
+        LinearLayout layout;
+        ImageView sendButton;
+        EditText messageArea;
+        ScrollView scrollView;
 
-    public class ChatMessage {
 
-        private String messageText;
-        private String messageUser;
-        private long messageTime;
 
-        public ChatMessage(String messageText, String messageUser) {
-            this.messageText = messageText;
-            this.messageUser = messageUser;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(activity_message);
 
-            // Initialize to current time
-            messageTime = new Date().getTime();
-        }
+            layout = (LinearLayout)findViewById(R.id.layout1);
+            sendButton = (ImageView)findViewById(R.id.sendButton);
+            messageArea = (EditText)findViewById(R.id.messageArea);
+            scrollView = (ScrollView)findViewById(R.id.scrollView);
 
-        public ChatMessage(){
+            Firebase.setAndroidContext(this);
+            reference1= new Firebase(User.class.getName() + " " /* + User.class.chatWith */);
+            referenc2 = new Firebase(/*User.class.chatWith + */ " " + User.class.getName());
 
-        }
+            DrawerLayout drawerLayout;
 
-        public String getMessageText() {
-            return messageText;
-        }
 
-        public void setMessageText(String messageText) {
-            this.messageText = messageText;
-        }
 
-        public String getMessageUser() {
-            return messageUser;
-        }
-
-        public void setMessageUser(String messageUser) {
-            this.messageUser = messageUser;
-        }
-
-        public long getMessageTime() {
-            return messageTime;
-        }
-
-        public void setMessageTime(long messageTime) {
-            this.messageTime = messageTime;
-        }
-        private void displayChatMessages() {
-            ListView listOfMessages = (ListView)findViewById(R.id.list_of_messages);
-
-            Query query = FirebaseDatabase.getInstance().getReference();
-            FirebaseListOptions<ChatMessage> options = new FirebaseListOptions.Builder<ChatMessage>()
-                    .setQuery(query, ChatMessage.class)
-                    .build();
-            adapter = new FirebaseListAdapter<ChatMessage>(options) {
+            sendButton.setOnClickListener(new View.OnClickListener() {
                 @Override
-                protected void populateView(View v, ChatMessage model, int position) {
+                public void onClick(View v) {
+                    String messageText = messageArea.getText().toString();
+                    //TextView name = (TextView) findViewById(id.name);
 
-            //    }
-            //};
+                    if(!messageText.equals("")){
+                        Map<String, String> map = new HashMap<String, String>();
+                        map.put("message", messageText);
+                        map.put("user", FirebaseDatabase.class.getName());
+                    }
 
-            //adapter = new FirebaseListAdapter<ChatMessage>(FirebaseDatabase.getInstance().getReference(),
-            //        ChatMessage.class, R.layout.activity_message, this) {
-            //    @Override
-            //    protected void populateView(View v, ChatMessage model, int position) {
-                    // Get references to the views of message.xml
-                    TextView messageText = (TextView)v.findViewById(R.id.message_text);
-                    TextView messageUser = (TextView)v.findViewById(R.id.message_user);
-                    TextView messageTime = (TextView)v.findViewById(R.id.message_time);
-
-                    // Set their text
-                    messageText.setText(model.getMessageText());
-                    messageUser.setText(model.getMessageUser());
-
-                    // Format the date before showing it
-                    messageTime.setText((CharSequence) DateFormat.getDateTimeInstance());
                 }
-            };
 
-            listOfMessages.setAdapter(adapter);
+            });
 
+            reference1.addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    Map map = dataSnapshot.getValue(Map.class);
+                    String message = map.get("message").toString();
+                    String userName = map.get("user").toString();
+
+                    if(userName.equals(User.class.getName())){
+                        addMessageBox("You:-\n" + message, 1);
+                    }
+                    else{
+                        addMessageBox(/*User.class.chatWith + */ ":-\n" + message, 2);
+                    }
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onCancelled(FirebaseError firebaseError) {
+
+                }
+            });
         }
 
-    }
+        public void addMessageBox(String message, int type){
+            TextView textView = new TextView(MessageActivity.this);
+            textView.setText(message);
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            lp.setMargins(0, 0, 0, 10);
+            textView.setLayoutParams(lp);
 
-
-
-    DrawerLayout drawerLayout;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_message);
-
-
-        FloatingActionButton fab =
-                (FloatingActionButton)findViewById(R.id.fab);
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EditText input = (EditText)findViewById(R.id.input);
-
-                // Read the input field and push a new instance
-                // of ChatMessage to the Firebase database
-                FirebaseDatabase.getInstance()
-                        .getReference()
-                        .push()
-                        .setValue(new ChatMessage(input.getText().toString(),
-                                FirebaseAuth.getInstance()
-                                        .getCurrentUser()
-                                        .getDisplayName())
-                        );
-
-                // Clear the input
-                input.setText("");
+            if(type == 1) {
+                textView.setBackgroundResource(R.drawable.rounded_corner1);
             }
-        });
+            else{
+                textView.setBackgroundResource(R.drawable.rounded_corner2);
+            }
 
-        drawerLayout = findViewById(R.id.drawer_layout);
+            layout.addView(textView);
+            scrollView.fullScroll(View.FOCUS_DOWN);
+        }
     }
 
     /*
