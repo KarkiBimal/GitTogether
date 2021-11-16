d47f8ad1557e51b3f91fd1cc48a993608a3cc479
brandonmaciel <brandonmaciel4@gmail.com>
2021-11-16 15:49:44 -0600

    added button to allow for posts user can now view profile from posts

--- app/src/main/java/com/example/gittogether/UserMessages.java
+++ app/src/main/java/com/example/gittogether/UserMessages.java
@@ -11,19 +11,19 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
+//import com.android.volley.Request;
+//import com.android.volley.RequestQueue;
+//import com.android.volley.Response;
+//import com.android.volley.VolleyError;
+//import com.android.volley.toolbox.StringRequest;
+//import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class Users extends AppCompatActivity {
+class Users extends AppCompatActivity {
     ListView usersList;
     TextView noUsersText;
     ArrayList<String> al = new ArrayList<>();
@@ -35,72 +35,72 @@ public class Users extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
 
-        usersList = (ListView)findViewById(R.id.usersList);
-        noUsersText = (TextView)findViewById(R.id.noUsersText);
-
-        pd = new ProgressDialog(Users.this);
-        pd.setMessage("Loading...");
-        pd.show();
-
-        String url = "https://gittogether-13f65-default-rtdb.firebaseio.com/Users";
-
-        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String s) {
-                doOnSuccess(s);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                System.out.println("" +  error);
-            }
-        }
-
-        );
-
-        RequestQueue rQueue = Volley.newRequestQueue(Users.this);
-        rQueue.add(request);
-
-         usersList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                UserDetails.chatWith = al.get(position);
-                startActivity(new Intent(Users.this, MessageActivity.class));
-            }
-        });
+//        usersList = (ListView)findViewById(R.id.usersList);
+//        noUsersText = (TextView)findViewById(R.id.noUsersText);
+//
+//        pd = new ProgressDialog(Users.this);
+//        pd.setMessage("Loading...");
+//        pd.show();
+//
+//        String url = "https://gittogether-13f65-default-rtdb.firebaseio.com/Users";
+//
+//        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+//            @Override
+//            public void onResponse(String s) {
+//                doOnSuccess(s);
+//            }
+//        }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                System.out.println("" +  error);
+//            }
+//        }
+//
+//        );
+//
+//        RequestQueue rQueue = Volley.newRequestQueue(Users.this);
+//        rQueue.add(request);
+//
+//         usersList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                UserDetails.chatWith = al.get(position);
+//                startActivity(new Intent(Users.this, MessageActivity.class));
+//            }
+//        });
     }
 
     public void doOnSuccess(String s){
-        try {
-            JSONObject obj = new JSONObject(s);
-
-            Iterator i = obj.keys();
-            String key = "";
-
-            while(i.hasNext()){
-                key = i.next().toString();
-
-                if(!key.equals(UserDetails.username)) {
-                    al.add(key);
-                }
-
-                totalUsers++;
-            }
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        if(totalUsers <=1){
-            noUsersText.setVisibility(View.VISIBLE);
-            usersList.setVisibility(View.GONE);
-        }
-        else{
-            noUsersText.setVisibility(View.GONE);
-            usersList.setVisibility(View.VISIBLE);
-            usersList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, al));
-        }
-
-        pd.dismiss();
+//        try {
+//            JSONObject obj = new JSONObject(s);
+//
+//            Iterator i = obj.keys();
+//            String key = "";
+//
+//            while(i.hasNext()){
+//                key = i.next().toString();
+//
+//                if(!key.equals(UserDetails.username)) {
+//                    al.add(key);
+//                }
+//
+//                totalUsers++;
+//            }
+//
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//
+//        if(totalUsers <=1){
+//            noUsersText.setVisibility(View.VISIBLE);
+//            usersList.setVisibility(View.GONE);
+//        }
+//        else{
+//            noUsersText.setVisibility(View.GONE);
+//            usersList.setVisibility(View.VISIBLE);
+//            usersList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, al));
+//        }
+//
+//        pd.dismiss();
     }
 }
