16238d6743b7f0ab6e57e5f6dea523f3af346d34
Daniel Stephens <49926015+danielstephens1@users.noreply.github.com>
2021-11-17 11:12:12 -0600

    small changes, commented messages out

--- app/src/main/java/com/example/gittogether/UserMessages.java
+++ app/src/main/java/com/example/gittogether/UserMessages.java
@@ -1,40 +1,40 @@
-package com.example.gittogether;
-
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+//package com.example.gittogether;
+//
+//import android.app.ProgressDialog;
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.AdapterView;
+//import android.widget.ArrayAdapter;
+//import android.widget.ListView;
+//import android.widget.TextView;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
 //import com.android.volley.Request;
 //import com.android.volley.RequestQueue;
 //import com.android.volley.Response;
 //import com.android.volley.VolleyError;
 //import com.android.volley.toolbox.StringRequest;
 //import com.android.volley.toolbox.Volley;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-
-class Users extends AppCompatActivity {
-    ListView usersList;
-    TextView noUsersText;
-    ArrayList<String> al = new ArrayList<>();
-    int totalUsers = 0;
-    ProgressDialog pd;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_message);
-
+//import org.json.JSONException;
+//import org.json.JSONObject;
+//
+//import java.util.ArrayList;
+//import java.util.Iterator;
+//
+//class Users extends AppCompatActivity {
+//    ListView usersList;
+//    TextView noUsersText;
+//    ArrayList<String> al = new ArrayList<>();
+//    int totalUsers = 0;
+//    ProgressDialog pd;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_message);
+//
 //        usersList = (ListView)findViewById(R.id.usersList);
 //        noUsersText = (TextView)findViewById(R.id.noUsersText);
 //
@@ -68,9 +68,9 @@ class Users extends AppCompatActivity {
 //                startActivity(new Intent(Users.this, MessageActivity.class));
 //            }
 //        });
-    }
-
-    public void doOnSuccess(String s){
+//    }
+//
+//    public void doOnSuccess(String s){
 //        try {
 //            JSONObject obj = new JSONObject(s);
 //
@@ -102,5 +102,5 @@ class Users extends AppCompatActivity {
 //        }
 //
 //        pd.dismiss();
-    }
-}
+//    }
+//}
